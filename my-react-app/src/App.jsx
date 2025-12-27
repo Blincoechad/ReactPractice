e54c85b4c6808ba2b09fa289742d0bb26bda6448
blincoechad <blincoechad@gmail.com>
2025-12-27 18:47:24 -0500

    added another button with onclick function/ Finish final touches for porfolio

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,10 +3,21 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [color, setColor] = useState('black');
+  const [color, setColor] = useState('rgb(20, 173, 187)');
+  const [textIndex, setTextIndex] = useState(0);
+
+  const textArray = ['Click The Button', 'Chad is...', 'Learning React'];
 
   const changeColors = () => {
-    setColor(prev => (prev === 'rgb(20, 173, 187)' ? 'rgb(212, 45, 193)' : 'rgb(20, 173, 187)'));
+    setColor(prev =>
+      prev === 'rgb(20, 173, 187)'
+        ? 'rgb(212, 45, 193)'
+        : 'rgb(20, 173, 187)'
+    );
+  };
+
+  const ChangeText = () => {
+    setTextIndex(prev => (prev + 1) % textArray.length);
   };
 
   return (
@@ -16,11 +27,15 @@ function App() {
       </div>
 
       <div className="buttonCards">
+        {/* CARD 1 */}
         <div className="cards card1">
           <p className="button-title" style={{ color }}>
             Click Button To Count!
           </p>
-          <button className="button1" onClick={() => setCount(count + 1)}>
+          <button
+            className="button1"
+            onClick={() => setCount(count + 1)}
+          >
             count is {count}
           </button>
           <p className="button-subTitle" style={{ color }}>
@@ -28,18 +43,38 @@ function App() {
           </p>
         </div>
 
+        {/* CARD 2 */}
         <div className="cards card2">
           <p className="button-title" style={{ color }}>
-            Click Button To 
+            Click Button To
           </p>
-          <button className="button2" onClick={changeColors}>Change buttons Text Color</button>
+          <button
+            className="button2"
+            onClick={changeColors}
+          >
+            Change buttons Text Color
+          </button>
           <p className="button-subTitle" style={{ color }}>
-            <em>A button that</em>
+            <em>A button that Changes Text Colors</em>
+          </p>
+        </div>
+
+        {/* CARD 3 */}
+        <div className="cards card3">
+          <p className="button-title" style={{ color }}>
+            {textArray[textIndex]}
+          </p>
+          <button
+            className=" cards button3"
+            onClick={ChangeText}
+          >
+            Click Me
+          </button>
+          <p className="button-subTitle" style={{ color }}>
+            <em>A button that Changes Text</em>
           </p>
         </div>
       </div>
-
-      
     </>
   );
 }
